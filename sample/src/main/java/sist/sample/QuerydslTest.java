3f2210128c4b9b93a768378213244ce7547bafc0
ted19horse <153588307+ted19horse@users.noreply.github.com>
2025-04-18 08:09:06 +0900

    Modify QuerydslTest to use EntityManager as a bean

--- sample/src/main/java/sist/sample/QuerydslTest.java
+++ sample/src/main/java/sist/sample/QuerydslTest.java
@@ -1,12 +1,29 @@
 package sist.sample;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
+@Service
 public class QuerydslTest {
-    // check
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    @Autowired
+    private JPAQueryFactory queryFactory;
+    
+    @Transactional
     public void test() {
-        EntityManager em = null;
-        JPAQueryFactory factory = new JPAQueryFactory(em);
+        // 직접 EntityManager 사용
+        System.out.println("EntityManager: " + entityManager);
+        
+        // Bean으로 등록된 JPAQueryFactory 사용
+        System.out.println("QueryFactory: " + queryFactory);
     }
 }
